cb6f56c113760ce88b2531f7b92b0f8d1e78f02f
supmatch <45656710+supmatch@users.noreply.github.com>
2019-01-03 15:45:15 +0800

    Update UseProxyIp.py

--- UseProxyIp.py
+++ UseProxyIp.py
@@ -13,20 +13,23 @@ def get_ip_lists(num=1):
 		ip = PyQuery(items)('td:nth-child(2)').text()
 		port = PyQuery(items)('td:nth-child(3)').text()
 		ip_http = PyQuery(items)('td:nth-child(6)').text()
-
-		if ip_http == 'HTTPS':
-			key = 'https'
-		else:
+		#这里的判断条件根据爬虫要访问的域名设定，访问https就用https的代理
+		if ip_http == 'HTTP':
 			continue
+		else:
+			key = 'https'
 		value = '{}://{}:{}'.format(key, ip, port)
 		proxy = {
 			key: value
 		}
 		try:
+			#headers看情况可以多弄几个进行随机，不然n多请求全是同样的headers可能会被反爬虫干掉
 			response = requests.get('https://www.supmatch.xyz/', headers=headers, proxies=proxy, timeout=2)
-			print('{}---{}'.format(ip_list,response.status_code))
+			s = requests.session()
+			print('{}---{}'.format(ip,response.status_code))
+			s.keep_alive = False
 		except:
 			print('timeout')
-			
+#这里是循环页码，先跑个10页的ip	
 for num in range(1,11):
 	get_ip_lists(num)
